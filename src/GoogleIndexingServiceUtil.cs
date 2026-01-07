938e8d3fe68980b2774faf8cfaaceb75ec1ada53
Jake Soenneker <jake@soenneker.com>
2026-01-06 19:35:42 -0600

    Deps

--- src/GoogleIndexingServiceUtil.cs
+++ src/GoogleIndexingServiceUtil.cs
@@ -1,55 +1,49 @@
+using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
+using Soenneker.Extensions.ValueTask;
 using Soenneker.Google.Credentials.Abstract;
 using Soenneker.Google.IndexingService.Abstract;
-using Soenneker.Utils.SingletonDictionary;
 using System.Threading;
 using System.Threading.Tasks;
-using Google.Apis.Auth.OAuth2;
-using Soenneker.Extensions.ValueTask;
+using Soenneker.Dictionaries.SingletonKeys;
 
 namespace Soenneker.Google.IndexingService;
 
 /// <inheritdoc cref="IGoogleIndexingServiceUtil"/>
-public sealed class GoogleIndexingServiceUtil: IGoogleIndexingServiceUtil
+public sealed class GoogleIndexingServiceUtil : IGoogleIndexingServiceUtil
 {
-    private readonly SingletonDictionary<global::Google.Apis.Indexing.v3.IndexingService> _indexingServices;
+    private static readonly string[] _scopes = ["https://www.googleapis.com/auth/indexing"];
+
+    private readonly IGoogleCredentialsUtil _googleCredentialsUtil;
+    private readonly SingletonKeyDictionary<string, global::Google.Apis.Indexing.v3.IndexingService> _indexingServices;
 
     public GoogleIndexingServiceUtil(IGoogleCredentialsUtil googleCredentialsUtil)
     {
-        _indexingServices = new SingletonDictionary<global::Google.Apis.Indexing.v3.IndexingService>(async (filename, token) =>
-        {
-            string[] scopes = ["https://www.googleapis.com/auth/indexing"];
-            ICredential credential = await googleCredentialsUtil.Get(filename, scopes, token).NoSync();
+        _googleCredentialsUtil = googleCredentialsUtil;
 
-            return new global::Google.Apis.Indexing.v3.IndexingService(new BaseClientService.Initializer
-            {
-                HttpClientInitializer = credential
-            });
+        // method group -> no closure capture
+        _indexingServices = new SingletonKeyDictionary<string, global::Google.Apis.Indexing.v3.IndexingService>(CreateIndexingService);
+    }
+
+    private async ValueTask<global::Google.Apis.Indexing.v3.IndexingService> CreateIndexingService(string filename, CancellationToken token)
+    {
+        ICredential credential = await _googleCredentialsUtil.Get(filename, _scopes, token)
+                                                             .NoSync();
+
+        return new global::Google.Apis.Indexing.v3.IndexingService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential
         });
     }
 
-    public ValueTask<global::Google.Apis.Indexing.v3.IndexingService> Get(string fileName, CancellationToken cancellationToken = default)
-    {
-        return _indexingServices.Get(fileName, cancellationToken);
-    }
+    public ValueTask<global::Google.Apis.Indexing.v3.IndexingService> Get(string fileName, CancellationToken cancellationToken = default) =>
+        _indexingServices.Get(fileName, cancellationToken);
 
-    public ValueTask Remove(string fileName, CancellationToken cancellationToken = default)
-    {
-        return _indexingServices.Remove(fileName, cancellationToken);
-    }
+    public ValueTask Remove(string fileName, CancellationToken cancellationToken = default) => _indexingServices.Remove(fileName, cancellationToken);
 
-    public void RemoveSync(string fileName, CancellationToken cancellationToken = default)
-    {
-        _indexingServices.RemoveSync(fileName, cancellationToken);
-    }
+    public void RemoveSync(string fileName, CancellationToken cancellationToken = default) => _indexingServices.RemoveSync(fileName, cancellationToken);
 
-    public void Dispose()
-    {
-        _indexingServices.Dispose();
-    }
+    public void Dispose() => _indexingServices.Dispose();
 
-    public ValueTask DisposeAsync()
-    {
-        return _indexingServices.DisposeAsync();
-    }
+    public ValueTask DisposeAsync() => _indexingServices.DisposeAsync();
 }
